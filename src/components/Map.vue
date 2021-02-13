ff0055336f7aa65350a8ce595b90fe8028283d42
Wells <brett.wells@uleth.ca>
2021-02-13 14:20:34 -0700

    add marker functionality

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,7 +3,25 @@
 </template>
 
 <script>
+import MarkerClusterer from '@google/markerclusterer';
+
 import gmapsInit from '../utils/gmaps';
+
+const locations = [
+  {
+    position: {
+      lat: 48.160910,
+      lng: 16.383330,
+    },
+  },
+  {
+    position: {
+      lat: 48.174270,
+      lng: 16.329620,
+    },
+  },
+  // ...
+];
 
 export default {
   name: 'Map',
@@ -21,6 +39,24 @@ export default {
         map.setCenter(results[0].geometry.location);
         map.fitBounds(results[0].geometry.viewport);
       });
+
+      const markerClickHandler = (marker) => {
+        map.setZoom(13);
+        map.setCenter(marker.getPosition());
+      };
+
+      const markers = locations
+        .map((location) => {
+            const marker = new google.maps.Marker({ ...location, map });
+            marker.addListener('click', () => markerClickHandler(marker));
+
+            return marker;
+          });
+
+        new MarkerClusterer(map, markers, {
+          imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+          });
+
     } catch (error) {
       console.error(error);
     }
