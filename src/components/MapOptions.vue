4014a552b458933aba610ab60c1d9a24a440b500
kenzo44 <kenzo44@users.noreply.github.com>
2021-02-13 23:14:46 -0700

    Added font awesome and added css to legend

--- src/components/MapOptions.vue
+++ src/components/MapOptions.vue
@@ -1,23 +1,11 @@
 <template>
   <fieldset class="map-options">
-    <legend>OneMap</legend>
-    <label>Filter Options</label><br/><hr>
+    <legend><i class="fas fa-eye"></i> YYC Watch Tower</legend>
+    <label>With Watch Tower, preview crimes that have occured and are occuring in Calgary in realtime.</label><br/><br/>
     <label><input v-model="type" type="radio" id="active" name="type" value="active" /> COVID-19 Cases</label><br />
     <label><input v-model="type" type="radio" id="confirmed" name="type" value="confirmed" /> Crime</label><br />
     <label><input v-model="type" type="radio" id="recovered" name="type" value="recovered" /> Hospitals</label><br />
     <label><input v-model="type" type="radio" id="deaths" name="type" value="deaths" /> Schools</label>
- <table>
-    <thead>
-      <tr><th>title</th></tr>
-      </thead>
-    <tbody>
-      <tr v-for="feature in features" v-bind:key="feature">
-       <td>
-         {{feature.properties.date}}
-         </td>
-        </tr>
-    </tbody>
-    </table>
   </fieldset>
 
 </template>
@@ -41,16 +29,22 @@ export default {
   border: none;
   font-family: 'Roboto', sans-serif;
   position: fixed;
-  width: 180px;
+  width: 220px;
   top: 120px;
   left: 10px;
   padding: 20px;
-  background: white;
+  background: rgb(255,255,255,0.8);
   border-radius: 5px;
-  font-size: large;
+  font-size: 18px;
+  border-radius: 5px;
+}
+hr{
+  border: 1px solid #333;
 }
 legend{
-  font-size: 18px;
+  font-size: 23px;
+  width: 100%;
+  text-align: center;
 }
 input {
   margin-bottom: 10px;
@@ -58,7 +52,7 @@ input {
 .map-options legend {
   background: white;
   font-weight: bold;
-  padding: 5px 10px;
+  padding: 10px 2px;
   border-radius: 10px;
 }
 </style>
