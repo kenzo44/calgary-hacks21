24f5f845826df438d11d8d41989a61be46506e8e
Wells <brett.wells@uleth.ca>
2021-02-13 18:50:15 -0700

    add firebase stuff to Map.vue

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -7,9 +7,40 @@ import MarkerClusterer from '@google/markerclusterer';
 
 import gmapsInit from '../utils/gmaps';
 
-import db from '../firebase';
+import Firebase from 'firebase'
+let config={
+  apiKey: "AIzaSyDtCUmTAoiHi6CAWHV4aNy6T8JT6KP1l50",
+    authDomain: "calgarycrime-9c9d0.firebaseapp.com",
+    databaseURL: "https://calgarycrime-9c9d0-default-rtdb.firebaseio.com",
+    projectId: "calgarycrime-9c9d0",
+    storageBucket: "calgarycrime-9c9d0.appspot.com",
+    messagingSenderId: "845317394150",
+    appId: "1:845317394150:web:6f25e93fa86a69ef863527"
+}
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let featuresRef = db.ref('features')
 
-console.log(db);
+console.log(featuresRef);
+
+var ref = Firebase.database().ref("features/9099/properties");
+
+var ref1 = Firebase.database().ref("features");
+ref1.once("value")
+  .then(function(snapshot) {
+    var numChild = snapshot.numChildren();
+    console.log(numChild);
+});
+
+ref.once("value")
+  .then(function(snapshot) {
+    var id = snapshot.child("id").val();
+    var lat = snapshot.child("lat").val();
+    var long = snapshot.child("long").val();
+    console.log(id);
+    console.log(lat);
+    console.log(long);
+  });
 
 const locations = [
   {
